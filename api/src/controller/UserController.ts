ec0aa6278d1a5daada26114a1fdd713477a24eef
PedroSantana23 <ph.silva.santana@gmail.com>
2024-11-14 11:58:29 -0300

    Autenticação

--- api/src/controller/UserController.ts
+++ api/src/controller/UserController.ts
@@ -1,16 +1,29 @@
 import { Request, Response } from "express";
 import { prisma } from "../utils/prisma";
+import { hash } from "bcryptjs";
 
 
 export class UserController {
+    async index(req: Request, res: Response) {
+        const users = await prisma.user.findMany();
+        return res.json({ users });
+    }
+
     async create(req: Request, res: Response) {
         const  { name, email, password } = req.body;
 
+        const userExists = await prisma.user.findUnique({ where: { email } });
+
+        if(userExists) {
+            return res.json({ error: "Usuário já existe" });
+        }
+
+        const hash_password = await hash(password, 8);
         const user = await prisma.user.create({
             data: {
                 name,
                 email,
-                password
+                password: hash_password,
             },
         });
 
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { UserController } from "./controller/UserController";
+import { AuthController } from "./controller/AuthController";
+import { AuthMiddlewares } from "./middlewares/auth";
 
 const usercontroller = new UserController();
+const authcontroller = new AuthController();
 
 export const router = Router();
 
 router.post("/create", usercontroller.create);
+router.get("/index", AuthMiddlewares, usercontroller.index);
+router.post("/auth", authcontroller.authenticate);
