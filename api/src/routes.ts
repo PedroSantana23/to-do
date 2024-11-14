010679106fe6a9d5b244ef153061419389aa2580
PedroSantana23 <ph.silva.santana@gmail.com>
2024-11-13 21:14:12 -0300

    Ínicio da API

---
+++ api/src/routes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { UserController } from "./controller/UserController";
+
+const usercontroller = new UserController();
+
+export const router = Router();
+
+router.post("/create", usercontroller.create);
